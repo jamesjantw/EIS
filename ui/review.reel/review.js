9226cd9e88affd3411bb8b379a1e0dad55ee43b1
jamesjantw <jamesjantw@gmail.com>
2015-04-21 07:04:26 +0000

    Add component Review

---
+++ ui/review.reel/review.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/review.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class Review
+ * @extends Component
+ */
+exports.Review = Component.specialize(/** @lends Review# */ {
+    constructor: {
+        value: function Review() {
+            this.super();
+        }
+    }
+});
